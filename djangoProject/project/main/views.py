c074438e9a1e81104a3309fcc2722640998fd144
JinS <dkdlfjs7894@naver.com>
2022-02-24 10:03:22 +0900

    hjs12

--- djangoProject/project/main/views.py
+++ djangoProject/project/main/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render
-
+from main.crawling import crawling
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
 import datetime
 
+crawling_tmp = crawling()
+
 def homeView(request):
-    return render(request,'home.html',None)
+    context = {
+        'images': crawling_tmp[0],
+        'urls': crawling_tmp[1],
+        'status': crawling_tmp[2],
+        'n': range(len(crawling_tmp[0])),
+    }
+    return render(request, 'home.html', context)
 
 def teamView(request):
     return render(request,'team.html',None)
 
 def joinView(request):
     return render(request,'join.html',None)
+
+def editView(request):
+    return render(request,'edit.html',None)
+
+def loginView(request):
+    return render(request,'login.html',None)
+
+def logoutView(request):
+    return render(request,'logout.html',None)
 
 def qnaView(request):
     return render(request,'qna.html',None)
